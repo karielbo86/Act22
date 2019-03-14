0e30f4fcfaa9c3b9ffe63424306295c36ede7631
karielbo86 <karielbo86@gmail.com>
2019-03-14 11:00:52 +0100

    definition de la voiture V1 avec les mutateurs

--- main.java
+++ main.java
@@ -4,6 +4,12 @@ class Main{
 
 		Voiture V1 = new Voiture();
 		Voiture V2 = new Voiture("Renault", "Blanche", 4, 785963, "Elec");
-		
+
+		V1.setMarque("Peugeot");
+		V1.setCouleur("Noire");
+		V1.setPortes(3);
+		V1.setMatricule(456852);
+		V1.setTypeMoteur("Diesel");
+
 	}
 }
